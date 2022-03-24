43762ec8fe6ccc4c00182180ab0c6f59bc9cc893
dglee <ldg6153@gmail.com>
2022-03-24 23:56:49 +0900

    UPDATE SpringConfig comment

--- src/main/java/DevDglee/hellospring/SpringConfig.java
+++ src/main/java/DevDglee/hellospring/SpringConfig.java
@@ -19,7 +19,11 @@ public class SpringConfig {
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
+    //다른 코드를 손대지 않고 구현체만 바꿨을 뿐인데, 그대로 쓸 수 있다. 데이터를 DB에 저장하므로 서버를 다시 실행해도 데이터가 안전하게 저장된다.
+    // 다형성을 활용한 것, 구현체 바꿔끼기 - 스프링은 이것을 편하게 해주게 도와주고 있다.
+    // 과거에는, 의존성이 높아 하나 고치면 다른것들도 함께 고쳐야했다.
+    // 어쎔블리, 조립하는쪽만 손대면 다른코드는 손대지 않아도 된다.
+    //개방 - 폐쇄 원칙( OCP, Open-closed Principle ) 확장에는 열려있고, 수정(변경)에는 닫혀있다.
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
